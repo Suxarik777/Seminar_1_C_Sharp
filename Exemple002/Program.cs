9f2d9a2c062be899834ec642753a0b55b95ab29c
Suxarik777 <gultcev@mail.ru>
2022-04-11 11:33:51 +0300

    Задача 2 на определение квадрата числа

---
+++ Exemple002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Задайте первое число, квадрат которого мы будем проверять по отношению ко второму числу: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте второе число :");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if(numberA * numberA == numberB)
+{
+    Console.WriteLine("Да заданое вами первое число, является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Нет эти числа не имеют ни какой связи");
+}
