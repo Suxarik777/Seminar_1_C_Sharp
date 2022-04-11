16e213235d359d9b8269250953e866cd03d3d5a6
Suxarik777 <gultcev@mail.ru>
2022-04-11 12:13:46 +0300

    fly github

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите день недели (числа от 1 до 7): ");
+int number_day = Convert.ToInt32(Console.ReadLine());
+
+if(number_day > 7)
+{
+    Console.WriteLine("У нас всего 7 дней недели, введите числа от одного до семи");
+}
+else if(number_day == 1)
+{
+   Console.WriteLine("Понедельник"); 
+}
+else if(number_day == 2)
+{
+   Console.WriteLine("Вторник"); 
+}else if(number_day == 3)
+{
+   Console.WriteLine("Среда"); 
+}
+else if(number_day == 4)
+{
+   Console.WriteLine("Четверг"); 
+}
+else if(number_day == 5)
+{
+   Console.WriteLine("Пятница"); 
+}
+else if(number_day == 6)
+{
+   Console.WriteLine("Суббота"); 
+}
+else if(number_day == 7)
+{
+   Console.WriteLine("Воскресенье"); 
+}
