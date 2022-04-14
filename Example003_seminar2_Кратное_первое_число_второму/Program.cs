0bdc08ec4e162c9b7b91506916b034825cfbfbff
Suxarik777 <gultcev@mail.ru>
2022-04-14 12:30:19 +0300

    Добавил 4 задачи из Семинара 2

---
+++ Example003_seminar2_Кратное_первое_число_второму/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.");
+
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number1 % number2 == 0) // проверяем на остаток деление от деления числа1 на число2 и если в резульате 0 то кратно,  
+{
+    Console.WriteLine("Числа кратны");
+}
+else
+{
+    Console.WriteLine("Числа не кратно, остаток от делени");
+    Console.WriteLine(number1 % number2); // показываем остаток целых чисел так как контейнер int
+}
---
+++ Example004_seminar2_Кратное_одновременное_деление_на_7_и_23/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на
+//вход число и проверяет, кратно ли оно
+//одновременно 7 и 23.
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result1 = number % 7;
+int result2 = number % 23;
+
+if (result1 == 0 && result2 == 0)
+{
+    Console.WriteLine("Число " + number + " кратно числам 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число " + number + " НЕ кратно числам 7 и 23");
+}
